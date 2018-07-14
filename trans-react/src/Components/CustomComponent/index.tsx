4e0fcd4fb18ddc408891f110a73607c2d1cfaa6b
Sadri FERTANI <sadri.fertani@live.fr>
2018-07-15 01:11:12 +0200

    V 0.2 Eureka

--- trans-react/src/Components/CustomComponent/index.tsx
+++ trans-react/src/Components/CustomComponent/index.tsx
@@ -1,29 +1,51 @@
 import * as React from 'react';
+import { CustomFisrtChildComponent } from '../CustomFisrtChildComponent';
+import { CustomSecondChildComponent } from '../CustomSecondChildComponent';
 
 interface IProps {
     message?: string;
+    // children?: any[]; // React.ReactChild[];
 }
 
 export class CustomComponent extends React.Component<IProps> {
+    private comp1: CustomFisrtChildComponent;
+    private comp2: CustomSecondChildComponent;
+
     public constructor(props: IProps) {
         super(props);
+
+        React.Children.forEach(this.props.children, this.getChild);
+
+        // const children = React.Children.toArray(this.props.children);
+        // }
     }
+
+    public getChild = (child: React.ReactChild, index: number) => {
+        const instanceName = (child as any).type.name;
+
+        switch (instanceName) {
+            case CustomFisrtChildComponent.name:
+                this.comp1 = this.props.children![index] as CustomFisrtChildComponent;
+                break;
+            case CustomSecondChildComponent.name:
+                this.comp2 = this.props.children![index] as CustomSecondChildComponent;
+                break;
+        }
+    }
+
     public render() {
         return (
             <div className="jumbotron">
                 <h1>Test is strating...</h1>
                 <div className="row">
                     <div className="col-md-4">left top</div>
-                    <div className="col-md-4">middle top</div>
+                    <div className="col-md-4">{this.comp1}</div>
                     <div className="col-md-4">right top</div>
                 </div>
                 <div className="row">
                     <div className="col-md-4">left bottom</div>
                     <div className="col-md-4">middle bottom</div>
-                    <div className="col-md-4">right bottom</div>
-                </div>
-                <div className="row">
-                    {this.props.children}
+                    <div className="col-md-4">{this.comp2}</div>
                 </div>
             </div>
         );
